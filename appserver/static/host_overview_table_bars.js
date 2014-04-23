9c1263394c1da9bbc7d600ae96cf8289a6580838
Sanford Owings <churlish@gmail.com>
2014-04-23 08:43:04 -0700

    o Change the table to show the '% of capacity' as a bar with the text in the middle.

--- appserver/static/host_overview_table_bars.js
+++ appserver/static/host_overview_table_bars.js
@@ -8,13 +8,13 @@ require([
 
     var PercentBarCellRenderer = BaseCellRenderer.extend({
         canRender: function(cell) {
-            return (cell.field === 'Percent');
+            return (cell.field === '% of Capacity');
         },
 
         render: function($td, cell) {
-            $td.addClass('data-bar-cell').html(_.template('<div class="data-bar-wrapper"><div class="data-bar-right" style="width:<%- percent %>%"></div></div>', {
+            $td.addClass('data-bar-cell').html(_.template('<div class="data-bar-wrapper"> <%- value %>% <span class="data-bar-right" style="width:<%- percent %>%"></span></div>', {
                 percent: Math.min(Math.max(parseFloat(cell.value), 0), 100),
-//                value: parseFloat(cell.value)
+                value: parseFloat(cell.value)
             }));
 
         }
