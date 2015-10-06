aa2f73dca880f16d2fc52545fc98ebc92a779818
Sanford Owings <sowings@splunk.com>
2015-10-06 12:42:36 -0700

    o Merge table bar changes for retention overview and host overview.

--- appserver/static/retention_overview_table_bars.js
+++ appserver/static/retention_overview_table_bars.js
@@ -12,8 +12,14 @@ require([
         },
 
         render: function($td, cell) {
-            $td.addClass('data-bar-cell').html(_.template('<div class="data-bar-wrapper"> <%- value %>% <span class="data-bar-right" style="width:<%- percent %>%"></span></div>', {
+            var pColor = "data-bar-under";
+            if (cell.value > 50) {
+                pColor = "data-bar-over";
+            }
+
+            $td.addClass('data-bar-cell').html(_.template('<div class="data-bar-wrapper"> <div style="position: absolute" class="data-bar-text-overlay <%-pColor %>"> <%- value %> % </div> <div class="data-bar-right" style="width:<%- percent %>%"></div></div>', {
                 percent: Math.min(Math.max(parseFloat(cell.value), 0), 100),
+                pColor: pColor,
                 value: parseFloat(cell.value)
             }));
 
