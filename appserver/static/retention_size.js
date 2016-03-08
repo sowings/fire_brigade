f66e9ffcf6514eeb61151f0ce96932aad2645e3f
Sanford Owings <sowings@splunk.com>
2016-03-08 10:40:38 -0800

    o Add ballast to the size-based retention dashbaords.

--- appserver/static/retention_size.js
+++ appserver/static/retention_size.js
@@ -17,4 +17,12 @@ require(['splunkjs/mvc/simplexml/ready!'], function(){
     // Now add in the desired group title. Styled in dashboard.css.
     group_header.prepend('<div class="panel-head"><h3>Cold Path</h3></div>');
 
+    var panel_of_interest = $('#retention_size_home_ballast_gauge').parent().parent().parent();
+    panel_of_interest.attr('style', "width: 25%");
+    
+    panel_of_interest = $('#retention_size_cold_ballast_gauge').parent().parent().parent();
+    panel_of_interest.attr('style', "width: 25%");
+
+    panel_of_interest = $('#retention_size_usage_vs_max').parent().parent().parent();
+    panel_of_interest.attr('style', "width: 50%");
 });
